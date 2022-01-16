8aee40b814a9b06b6a8d592cb9def8c0a7ac3d07
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-16 12:18:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const client = new Client({intents: 3});
 client.once("ready", () =>{
     console.log("Online");
 });
-client.login("OTI4NjIxNzY5ODI3NDQyNzA4.Ydbcag.f9wJmyrtPAksJFbwYB1UNUEJmHQ")
+client.login(process.env.BOT_TOKEN);
