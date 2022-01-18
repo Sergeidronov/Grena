a9b5c791d9ad14d8223f63cdf81e20d24fac996b
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-18 06:13:31 +0300

    Update index.js

--- Structures/index.js
+++ Structures/index.js
@@ -20,7 +20,102 @@ client.commands = new Collection();
     require(`./Handlers/${handler}`)(client, PG, Ascii);
 });
 
+client.on('messageCreate', async (message) => {
 
+    if (!message.content.startsWith(config.prefix)) return;
+	
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift();
+
+    if (command == "button") { 
+        const button = new MessageButton()
+            .setLabel("Archive")
+            .setStyle("PRIMARY")
+            .setCustomId("archive");
+        const buttons = new MessageActionRow()
+            .addComponents(button);
+        message.channel.send({content: `button`, components: [buttons]});
+    }
+    if (command == "select") {
+        const selectMenu = new MessageSelectMenu()
+            .setCustomId('select')
+            .setPlaceholder('Ничего не выбрано')
+            .addOptions([
+                {
+                    label: 'Опция 1',
+                    description: 'Описание первой опции',
+                    value: 'first_option'
+                },
+                {
+                    label: 'Опция 2',
+                    description: 'Описание второй опции',
+                    value: 'second_option'
+                }
+            ])
+            .setMaxValues(1);
+        const menu = new MessageActionRow()
+            .addComponents(selectMenu);
+        message.channel.send({content: `select menu`, components: [menu]});
+    }
+    if (command == "slash") {
+        message.guild.commands.create({
+            name: "slash",
+            description: "Описание тестовой Slash команды"
+        })
+        message.reply("Добавлена Slash команда");
+    }
+    if (command == "context") {
+        message.guild.commands.create({
+            name: "contextuser",
+            type: "USER"
+        })
+        
+        message.guild.commands.create({
+            name: "contextmessage",
+            type: "MESSAGE"
+        })
+        message.reply("Контекстное меню настроено");
+    }
+    if (command == "deletecommands") {
+        var commands = await message.guild.commands.fetch();
+        if (commands != undefined) commands.forEach((i) => {
+            i.delete();
+        })
+        message.reply("Команды удалены");
+    }
+});
+
+client.on('interactionCreate', async (interaction) => {
+    console.log(interaction);
+    if (interaction.isButton() && interaction.customId == "archive") {
+        interaction.reply({
+            content: `Ответ на кнопку`,
+            ephemernal: true
+        });
+    }
+    
+    if (interaction.isSelectMenu() && interaction.customId == "select") {
+        interaction.reply({
+            content: `Ну вот и ответ на селект: ${interaction.values[0]}!`,
+            ephemernal: true
+        });
+    }
+
+    if (interaction.isCommand() && interaction.commandName == "slash") {
+        interaction.reply({
+            content: `Ну вот и ответ на слеш команду ${interaction.commandName}!`,
+            ephemernal: true
+        });
+    }
+
+    if (interaction.isContextMenu()) {
+        interaction.reply({
+            content: `Ну вот и ответ на контекстное меню: ${interaction.commandName}!\ntargetType: ${interaction.targetType}\ntargetId: ${interaction.targetId}`,
+            ephemernal: true
+        });
+    }
+
+});
 
 
 
