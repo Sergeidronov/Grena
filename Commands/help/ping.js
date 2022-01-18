c13dc42e73dcea8f815c43fa0ac9a8321fb412c9
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-18 06:24:28 +0300

    1

--- Commands/help/ping.js
+++
@@ -1,9 +0,0 @@
-const { Client, Collection, Intents, MessageEmbed, MessageButton, MessageActionRow, MessageSelectMenu } = require("discord.js");;
-module.exports = {
-    name: "ping",
-    description: "Tiempo de respuesta",
-    roles: ["admin", "coder"],
-    async execute(client, message, args, discord) {
-      message.channel.send("!PONG 😄");
-    },
-  };
