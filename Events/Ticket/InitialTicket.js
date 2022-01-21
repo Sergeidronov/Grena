d4809e55f1c65457629c6bd4823a29502d65ad5e
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-21 10:24:24 +0300

    Update InitialTicket.js

--- Events/Ticket/InitialTicket.js
+++ Events/Ticket/InitialTicket.js
@@ -88,10 +88,7 @@ module.exports = {
                  }, 1 * 5000);
              });
 
-             interaction.reply({
-              content: `${member} your ticket has been created ${channel}`, 
-              ephemeral: true, 
-            });
+
         });
     },
 };
