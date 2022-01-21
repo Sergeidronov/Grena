58029a31116ebb3af333f2ba97e36ef7f528dae6
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-21 07:21:36 +0300

    Update suggestdbs.js

--- Memory/Schemas/suggestdbs.js
+++ Memory/Schemas/suggestdbs.js
@@ -1,7 +1,7 @@
 const { model, Schema } = require("mongoose");
 
-module.exports = model ("suggestdbs",   new Schema({
-        GuildID: String,
-        MessageID: String,
-        Details: Array
-    }))
+module.exports = model("suggestdbs", new Schema({
+  GuildID: String,
+  MessageID: String,
+  Details: Array
+}));
