f7ddddf65d5a8d2fd3485614dab214003ac07b3b
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-21 08:01:35 +0300

    Create InitialTicket.js

---
+++ Events/Ticket/InitialTicket.js
@@ -0,0 +1,95 @@
+const {
+    ButtonInteraction,
+    MessageEmbed,
+    MessageActionRow,
+    MessageButton
+} = require("discord.js");
+const DB = require("../../Memory/Schemas/Ticket");
+const {PARENTID, EVERYONEID} = require("../../Structures/config.json");
+
+module.exports = {
+    name: "interactionCreate",
+    /**
+     * @param {ButtonInteraction}
+     */
+
+    async execute(interaction) {
+        if(!interaction.isButton()) return;
+        const { guild, member, customId} = interaction
+
+        if(!["player", "bug", "other"].includes(customId)) return;
+
+        const ID = Math.Floor(Math.random() * 90000) + 10000;
+
+        await guild.channels.create(`${customId + "-" + ID}`, {
+            type: "GUILD_TEXT",
+            parent: PARENTID,
+            permissionOverwrites: [
+                {
+                    id: member.id,
+                    allow: ["SEND_MESSAGE", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                },
+                {
+                    id: EVERYONEID,
+                    deny: ["SEND_MESSAGE", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                },
+            ],
+        }).then(async(channel) => {
+            await DB.create({
+                GuildID: guild.id,
+                MemberID: member.id,
+                TicketID: ID,
+                ChannelID: channel.id,
+                Closed: false,
+                Locked: false,
+                type: customId,
+            });
+
+            const Embed = new MessageEmbed()
+            .setAuthor(`${guild.name} | Ticket: ${ID}`,
+            guild.iconURL({dynamic: true})
+            )
+            .setDescription("Please wait patiently for a response from the Staff")
+            .setFooter("The button")
+    
+            const Buttons = new MessageActionRow();
+    
+            Buttons.addComponents(
+                new MessageButton()
+                .setCustomId("close")
+                .setLabel("player report")
+                .setStyle("PRIMARY")
+                .setEmoji("😀"),
+                new MessageButton()
+                .setCustomId("lock")
+                .setLabel("bug")
+                .setStyle("SECONDARY")
+                .setEmoji("😇"),
+                new MessageButton()
+                .setCustomId("unlock")
+                .setLabel("Bug")
+                .setStyle("SUCCESS")
+                .setEmoji("😈"),
+    
+            );
+    
+            channel.send({ 
+             embeds: [Embed], 
+             components: [Buttons]});
+             await channel.
+             send({content: `${member} Here choto`})
+             .then((m =>{
+                 setTimeout(() => {
+                     m.delete().catch(() => {});
+                 }, 1 * 5000);
+
+                 
+             }));
+    
+             interaction.reply({content: `${member} your ticket has been created ${channel}`})
+        });
+
+
+    }
+}
+
