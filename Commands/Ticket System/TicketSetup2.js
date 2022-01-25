b8842de14bdfde74d99192e901757cdf3ff997c8
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-25 05:27:07 +0300

    1

---
+++ Commands/Ticket System/TicketSetup2.js
@@ -0,0 +1,145 @@
+const {
+    MessageEmbed,
+    CommandInteraction,
+    MessageActionRow,
+    MessageButton,
+} =require("discord.js");
+const DB = require("../../Memory/Schems/TicketSetup");
+
+module.exports = {
+    name: "ticketsetup2",
+    description: "Setup ticket",
+    permission: "ADMINISTRATOR",
+    options: [
+        {
+        name: "channel", 
+        description: "Select the ticket creation channel", 
+        required: true,
+        type: "CHANNEL", 
+        channelTypes: ["GUILD_TEXT"],
+    },
+    {
+        name: "category", 
+        description: "Select the ticket channels creation category", 
+        required: true,
+        type: "CHANNEL", 
+        channelTypes: ["GUILD_CATEGORY"],
+    },
+    {
+        name: "transcripts", 
+        description: "Select the transcripts", 
+        required: true,
+        type: "CHANNEL", 
+        channelTypes: ["GUILD_TEXT"],
+    },
+    {
+        name: "handlers", 
+        description: "Select the ticket handlers role", 
+        required: true,
+        type: "ROLE", 
+    },
+    {
+        name: "everyone", 
+        description: "Provide the @everyone role", 
+        required: true,
+        type: "ROLE", 
+    },
+    {
+        name: "description", 
+        description: "Set the description", 
+        required: true,
+        type: "STRING", 
+    },
+    {
+        name: "firstbutton", 
+        description: "Give your first button", 
+        required: true,
+        type: "STRING", 
+    },
+    
+    ],
+
+    /**
+     * @param {CommandInteraction} interaction
+     */
+    async execute(interaction ) {
+        const {guild, options} = interaction;
+
+
+        try {
+            const Channel = options.getChannel("channel");
+            const Category = options.getChannel("category");
+            const Transcripts = options.getChannel("transcripts");
+            const Handlers = options.getRole("handlers");
+            const Everyone = options.getRole("everyone");
+
+            const Description = options.getString("description");
+
+            const Button1 = options.getString("firstbutton").split(",");
+
+            const Emoji1 = Button1[1];
+
+
+
+
+            await DB.findOneAndUpdate(
+                {GuildID: guild.id},
+                 {
+                     Channel: Channel.id,
+                     Category: Category.id,
+                      Transcripts: Transcripts.id,
+                       Handlers: Handlers.id,
+                       Everyone: Everyone.id,
+                       Description: Description,
+                       Buttons: [Button1[0], ],
+                    },
+                    {
+                        new: true,
+                        upsert: true,
+                    });
+                    
+            
+                    const Button = new MessageActionRow()
+            
+                    Button.addComponents(
+                        new MessageButton()
+                        .setCustomId(Button1[0])
+                        .setLabel(Button1[0])
+                        .setStyle("PRIMARY")
+                        .setEmoji(Emoji1),
+            
+                    );
+
+
+                    const Embed = new MessageEmbed() 
+                    .setAuthor({
+                        name: guild.name + " | Ticketing System",
+                        iconURL: guild.iconURL ({dynamic: true}),
+                    })
+                    .setDescription(Description)
+                    .setColor("RED");
+                    
+
+                    await guild.channels.cache.get(Channel.id)
+                    .send({embeds: [Embed], components: [Button]});
+            
+                    interaction.reply({content: "Done", ephemeral: true});
+ 
+
+        } catch (err) {
+            const errEmbed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(`⛔ | An error occured while setting up your ticket system\n**what to make sure off?** 
+            1.Make sure none  of your button name are duplicated
+            2.Make sure you can use this formated for your button => Name, Emoji
+            3.Make sure your button name do not exceed 200 characted
+            4.Make sure your button emoji are actually emoji`
+            );
+            console.log(err);
+            interaction.reply({embeds: [errEmbed]});
+        }
+
+       
+    },
+};
+
---
+++ Events/Ticket/TicketOps2.js
@@ -0,0 +1,159 @@
+const { ButtonInteraction, MessageEmbed } = require("discord.js");
+const { createTranscript } = require("discord-html-transcripts");
+const DB = require("../../Memory/Schems/Tickets");
+const TicketSetupData = require("../../Memory/Schems/TicketSetup");
+
+
+
+module.exports = {
+    name: "interactionCreate",
+    /**
+     * 
+     * @param {ButtonInteraction} interaction
+     */
+    async execute(interaction) {
+        if (!interaction.isButton()) return;
+        const { guild, customId, channel, member } = interaction;
+        if (!["close", "lock", "unlock", "claim"].includes(customId)) return;
+ 
+        const TicketSetup = await TicketSetupData.findOne({GuildID: guild.id});
+        if(!TicketSetup)
+        return interaction.reply({
+            content: "Данные для этой системы устарели.",
+        });
+
+        if (!member.roles.cache.find((r) => r.id === TicketSetup.Handlers))
+        return interaction.reply({ 
+            content: "Недостаточно прав для использования.",
+            ephemeral: true,
+});
+    
+
+        const Embed = new MessageEmbed().setColor("BLUE");
+
+        DB.findOne({ ChannelID: channel.id }, async(err, docs) => {
+            if (err) throw err;
+            if (!docs) return interaction.reply({
+                content: "Никаких данных, связанных с этим билетом, найдено не было, пожалуйста, удалите руководство,",
+                ephemeral: true,
+            });
+            switch (customId) {
+                case "lock":
+                    if (docs.Locked == true)
+                        return interaction.reply({
+                            content: "Этот тикет уже закрыт",
+                            ephemeral: true,
+                        });
+                    await DB.updateOne({ ChannelID: channel.id }, { Locked: true });
+                    Embed.setDescription("🔒 | Тикет был закрыт");
+                    
+                    docs.MembersID.forEach((m) => {
+                        channel.permissionOverwrites.edit(m, {
+                            SEND_MESSAGES: false,
+                        });
+
+                    })
+
+
+
+                    interaction.reply({ embeds: [Embed] });
+                    break;
+                case "unlock":
+                    if (docs.Locked == false)
+                        return interaction.reply({
+                            content: "Этот тикет уже открыт",
+                            ephemeral: true,
+                        });
+                    await DB.updateOne({ ChannelID: channel.id }, { Locked: false });
+                    
+                    Embed.setDescription("🔓 | Тикет был открыт");
+                    docs.MembersID.forEach((m) => {
+                        channel.permissionOverwrites.edit(m, {
+                            SEND_MESSAGES: true, 
+                        });
+
+                    })
+                    interaction.reply({ embeds: [Embed] });
+                    break;
+                case "close":
+                    if (docs.Closed == true)
+                        return interaction.reply({
+                             content: "Тикет уже закрыт, пожалуйста подождите", 
+                             ephemeral: true, });
+                    const attachment = await createTranscript(channel, {
+                        limit: -1,
+                        returnBuffer: false,
+                        fileName: `${docs.Type} - ${docs.TicketID}.html`,
+                    });
+                    await DB.updateOne({ ChannelID: channel.id }, { Closed: true });
+
+                    const MEMBER = guild.members.cache.get(docs.MemberID);
+                    const Message = await guild.channels.cache
+            .get(TicketSetup.Transcripts)
+            .send({
+              embeds: [
+                Embed.setTitle(`Тикет закрыт`).addFields([
+                  {
+                    name: "Тикет айди",
+                    value: `${docs.TicketID}`,
+                    inline: true,
+                  },
+                  {
+                    name: "Тип",
+                    value: `${docs.Type}`,
+                    inline: true,
+                  },
+                  {
+                    name: "Открыл",
+                    value: `<@!${docs.MembersID[0]}>`,
+                    inline: true,
+                  },
+                  {
+                    name: "Время открытия",
+                    value: `<t:${docs.OpenTime}:R>`,
+                    inline: true,
+                  },
+                  {
+                    name: "Время закрытия",
+                    value: `<t:${parseInt(Date.now() / 1000)}:R>`,
+                    inline: true,
+                  },
+                  {
+                    name: "Принял",
+                    value: `<@!${docs.ClaimedBy}>`,
+                    inline: true,
+                  },
+                ]),
+              ],
+              files: [attachment],
+            });
+            
+
+                    interaction.reply({
+                        embeds: [Embed.setDescription(
+                            `The transcripts save [TRANSCRIPTS](${Message.url})`
+                        ),
+                    ]
+                    })
+
+                    setTimeout(() => {
+                        channel.delete();
+                    }, 10 * 1000);
+                    break;
+
+                    case "claim":
+                        if(docs.Claimed == true) 
+                            return interaction.reply({content: `Этот тикет уже был принят <@${docs.ClaimedBy}>`,
+                            ephemeral: true,
+                        });
+
+                        await DB.updateOne({ChannelID: channel.id}, {Claimed: true, ClaimedBy: member.id});
+
+                        Embed.setDescription(`🛄 | Тикет был принят ${member}`);
+                        interaction.reply({embeds: [Embed]});
+                        
+                        break;
+            }
+        });
+    },
+};
