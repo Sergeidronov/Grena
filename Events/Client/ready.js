be126c878782d7bec5db7fb1243006e6cded87da
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-25 07:58:44 +0300

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,7 +1,6 @@
 const { Client } = require("discord.js")
 const mongoose = require("mongoose");
 const {Database} = require("../../Structures/config.json");
-const client = require('discord-rich-presence')('180984871685062656');
 
 module.exports = {
     name: "ready",
@@ -12,9 +11,6 @@ module.exports = {
      */
     execute (client) {
 
-        
-    
-        
 
         if (!Database) return;
         mongoose.connect(Database, {
