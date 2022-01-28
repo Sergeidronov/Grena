75197397a8752085e002bf2be2d6e7c285068795
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-28 16:24:10 +0300

    1

--- Commands/Admins/guild-config.js
+++ Commands/Admins/guild-config.js
@@ -141,7 +141,7 @@ client.on("guildMemberEntered", (member) => {
 
 
 client.on("messageDelete", (message) => {
-    if(message.author.bot) return;
+    
         let happen = Math.floor(new Date().getTime()/1000.0)
     const LogChannel = interaction.options.getChannel('logs-channel'); // Replace with your channel id
 
