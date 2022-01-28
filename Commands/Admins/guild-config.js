0a24bb5668dd4197bbed0b8c1cae4976d60d3486
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-28 16:31:25 +0300

    1

--- Commands/Admins/guild-config.js
+++ Commands/Admins/guild-config.js
@@ -140,35 +140,21 @@ client.on("guildMemberEntered", (member) => {
 })
 
 
-client.on("messageDelete", (message) => {
-    
-        let happen = Math.floor(new Date().getTime()/1000.0)
+client.on("messageContentEdited", (message, oldContent, newContent) => {
+
     const LogChannel = interaction.options.getChannel('logs-channel'); // Replace with your channel id
-
-    const MessageDelete = new MessageEmbed()
-        .setColor('#36393f')
-        .setDescription(`A Message by ${message.author.tag} was deleted`)
-        .addFields({
-            name: 'Content',
-            value: `${message.content ? message.content : "None"}`.slice(0, 4096),
-            inline: false
-        }, {
-            name: 'Action performed',
-            value: `(<t:${happen}:R>)`,
-            inline: false
-        })
-
-        if(message.attachments.size >= 1) {
-            Log.addField(`Attachments:`, `${message.attachments.map(a => a.url)}`, true)
+    const MessageEdited = new MessageEmbed()
+        .setTitle('Message Edited')
+        .setColor('#2F3136')
+        .setDescription(`Message Edited from \`${oldContent}\` to \`${newContent}\``);
+ 
         
         return LogChannel.send({
             embeds: [MessageDelete]
         }
         )
 }
-}
 )
-  
 
 
 
