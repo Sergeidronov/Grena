74b3dc66344195fcc4fd65807315c507635f80e0
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-29 07:32:46 +0300

    Update guild-config.js

--- Commands/Admins/guild-config.js
+++ Commands/Admins/guild-config.js
@@ -162,7 +162,7 @@ client.on("messageContentEdited", (message, oldContent, newContent) => {
     const MessageEdited = new MessageEmbed()
         .setTitle('Сообщение изменено')
         .setColor('#2F3136')
-        .setDescription(`Сообщение  \*\*\`${oldContent}\` изменено на \`${newContent}\``);
+        .setDescription(`Сообщение  \*\*\`${oldContent}\` изменено на  \`${newContent}\``);
 
     return LogChannel.send({
         embeds: [MessageEdited]
