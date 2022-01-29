99f29bb68b42ec1864d19f37975108307cbefea2
Sergeidronov <rodionowa.yu@yandex.ru>
2022-01-29 09:34:27 +0300

    1

--- Memory/Schems/altlogs.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const altlogSchema = mongoose.Schema({
-    Guild: String,
-    Channel: String
-})
-
-module.exports = mongoose.model("altlogs", altlogSchema)
